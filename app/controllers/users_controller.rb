3d73e5604d32ad12b3dff164f358251159d3b44d
Aman Tank <amantank187@gmail.com>
2021-07-14 16:47:24 +0100

    User has its own profile page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+    before_action :set_user, only: [:show] 
+    before_action :authenticate_user!
+
+    def show 
+        
+    end 
+
+    private 
+
+    def set_user
+        @user = User.find(params[:id])
+    end 
+end
