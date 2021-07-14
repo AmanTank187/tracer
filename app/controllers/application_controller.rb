8e426c71c3307a2f47a563d86aa83c49a7be6971
Aman Tank <amantank187@gmail.com>
2021-07-14 13:35:44 +0100

    Added username to the devise form and can create a user with permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    #Lines 3,7-9 are implemented to permit a username when signing up
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+    
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 end
