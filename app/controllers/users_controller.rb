5a8d37f488bb682ecff2a333a8f909e9e332aa7b
Aman Tank <amantank187@gmail.com>
2021-07-27 14:42:53 +0100

    Added migration and established relationship between locations and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     before_action :set_user, only: [:show] 
     before_action :authenticate_user!
     def index 
-
+        
     end 
     
     def show 
