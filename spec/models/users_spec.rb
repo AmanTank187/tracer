7c7cd4a19ff9c838b07c26320bd79e67471a6b3a
Aman <Amantank187@gmail.com>
2021-08-24 16:41:44 +0100

    Added a user model test for creating a user successfully

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -4,5 +4,12 @@ RSpec.describe User, type: :model do
             user = User.new(username: "",email: "", password: "").save
             expect(user).to eq(false)
         end 
+
+        it "Saves the user" do 
+            user = User.new(username: "amantest",
+            email: "amantest@example.com", 
+            password: "amantest").save
+            expect(user).to eq(true)
+        end 
     end 
 end 
